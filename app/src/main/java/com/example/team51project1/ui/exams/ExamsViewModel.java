f82c3e821512701ebab7843acfa06af748670b40
sadhalan <sadhaleanisha@gmail.com>
2024-01-22 09:56:51 -0500

    all fragments - todo list

--- app/src/main/java/com/example/team51project1/ui/exams/ExamsViewModel.java
+++ app/src/main/java/com/example/team51project1/ui/exams/ExamsViewModel.java
@@ -1,16 +1,16 @@
-package com.example.team51project1.ui.dashboard;
+package com.example.team51project1.ui.exams;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class ExamsViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public ExamsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is exams fragment");
     }
 
     public LiveData<String> getText() {
