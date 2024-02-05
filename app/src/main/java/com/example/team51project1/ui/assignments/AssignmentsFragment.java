5ba2b65591556bb64e37532ca44f8be56d98f526
sadhalan <sadhaleanisha@gmail.com>
2024-02-04 20:27:37 -0500

    all buttons, classes, and assignments working

--- app/src/main/java/com/example/team51project1/ui/assignments/AssignmentsFragment.java
+++ app/src/main/java/com/example/team51project1/ui/assignments/AssignmentsFragment.java
@@ -4,6 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,13 +16,18 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.team51project1.Assignment;
+import com.example.team51project1.Course;
 import com.example.team51project1.R;
 import com.example.team51project1.databinding.FragmentAssignmentsBinding;
 import com.example.team51project1.ui.classes.ClassesFragment;
 
+import java.util.ArrayList;
+
 //does this make a change?
 public class AssignmentsFragment extends Fragment {
 
+    int selected;
     private FragmentAssignmentsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -36,11 +46,90 @@ public class AssignmentsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.homeButtonAS).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.homeButtonAssignments).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(AssignmentsFragment.this)
                         .navigate(R.id.action_navigation_assignments_to_navigation_home3);
+            }
+        });
+
+        selected = -1;
+
+        ListView courseListView = (ListView) view.findViewById(R.id.assignmentList);
+        ArrayList<Assignment> backingList = new ArrayList<>();
+        ArrayAdapter<Assignment> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, backingList);
+        courseListView.setAdapter(adapter);
+
+        EditText enterAssignment = view.findViewById(R.id.assignment_title);
+        EditText enterDueDate = view.findViewById(R.id.assignment_duedate);
+        EditText enterCourse = view.findViewById(R.id.assignment_course);
+
+        Button addAssignment = view.findViewById(R.id.addAssignment);
+
+
+        addAssignment.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String assignmentStr = enterAssignment.getText().toString();
+                String dueDateStr = enterDueDate.getText().toString();
+                String courseStr = enterCourse.getText().toString();
+                backingList.add(new Assignment(assignmentStr, dueDateStr, courseStr));
+                adapter.notifyDataSetChanged();
+
+                enterAssignment.setText("");
+                enterDueDate.setText("");
+                enterCourse.setText("");
+            }
+        });
+
+        Button edit = view.findViewById(R.id.editAssignment);
+        courseListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selected = position;
+                Assignment now = backingList.get(position);
+
+                enterAssignment.setText(now.getAssignmentName());
+                enterDueDate.setText(now.getAssignmentDueDate());
+                enterCourse.setText(now.getAssignmentCourse());
+
+                //Toast.makeText(Classes.this, now.getName() + " has been selected to be edited or deleted.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (selected != -1) {
+                    Assignment now = backingList.get(selected);
+                    now.setAssignmentName(enterAssignment.getText().toString());
+                    now.setAssignmentDueDate(enterDueDate.getText().toString());
+                    now.setAssociatedCourse(enterCourse.getText().toString());
+                    adapter.notifyDataSetChanged();
+
+                    enterAssignment.setText("");
+                    enterDueDate.setText("");
+                    enterCourse.setText("");
+                    selected = -1;
+                }
+            }
+        });
+
+        Button del = view.findViewById(R.id.deleteAssignment);
+        del.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (selected != -1) {
+                    backingList.remove(selected);
+                    adapter.notifyDataSetChanged();
+                    selected = -1;
+
+                    enterAssignment.setText("");
+                    enterDueDate.setText("");
+                    enterCourse.setText("");
+                }
             }
         });
 
--- app/src/main/java/com/example/team51project1/ui/classes/ClassesFragment.java
+++ app/src/main/java/com/example/team51project1/ui/classes/ClassesFragment.java
@@ -4,42 +4,42 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 //does this make a change?
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.team51project1.Course;
 import com.example.team51project1.R;
 import com.example.team51project1.databinding.FragmentClassesBinding;
-import com.example.team51project1.ui.home.HomeFragment;
+
+import java.util.ArrayList;
 
 public class ClassesFragment extends Fragment {
 
     private FragmentClassesBinding binding;
+    int selected;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        ClassesViewModel classesViewModel =
-                new ViewModelProvider(this).get(com.example.team51project1.ui.classes.ClassesViewModel.class);
-
 
         binding = FragmentClassesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        final TextView textView = binding.textDashboard;
-        classesViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
         return root;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         view.findViewById(R.id.homeButtonC).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -48,11 +48,96 @@ public class ClassesFragment extends Fragment {
             }
         });
 
-        view.findViewById(R.id.addEditClasses).setOnClickListener(new View.OnClickListener() {
+        selected = -1;
+
+        ListView courseListView = (ListView) view.findViewById(R.id.classList);
+        ArrayList<Course> backingList = new ArrayList<>();
+        ArrayAdapter<Course> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, backingList);
+        courseListView.setAdapter(adapter);
+
+        EditText enterClass = view.findViewById(R.id.course_title);
+        EditText enterTime = view.findViewById(R.id.course_time);
+        EditText enterInstructor = view.findViewById(R.id.course_instructor);
+        EditText enterLocation = view.findViewById(R.id.course_location);
+        EditText enterDaysOfWeek = view.findViewById(R.id.course_days);
+
+        Button addClass = view.findViewById(R.id.addClass);
+
+
+        addClass.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(ClassesFragment.this)
-                        .navigate(R.id.action_navigation_classes_to_navigation_add_class);
+                String classStr = enterClass.getText().toString();
+                String timeStr = enterTime.getText().toString();
+                String instructorStr = enterInstructor.getText().toString();
+                String locationStr = enterLocation.getText().toString();
+                String dayOfWeekStr = enterDaysOfWeek.getText().toString();
+                backingList.add(new Course(classStr,  instructorStr, locationStr, dayOfWeekStr, timeStr));
+                adapter.notifyDataSetChanged();
+
+                enterClass.setText("");
+                enterTime.setText("");
+                enterInstructor.setText("");
+                enterLocation.setText("");
+                enterDaysOfWeek.setText("");
+            }
+        });
+
+        Button edit = view.findViewById(R.id.editClass);
+        courseListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selected = position;
+                Course now = backingList.get(position);
+
+                enterClass.setText(now.getCourseName());
+                enterTime.setText(now.getCourseTime());
+                enterInstructor.setText(now.getCourseTeacher());
+                enterLocation.setText(now.getCourseBuilding());
+                enterDaysOfWeek.setText(now.getCourseDaysOfWeek());
+
+                //Toast.makeText(Classes.this, now.getName() + " has been selected to be edited or deleted.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (selected != -1) {
+                    Course now = backingList.get(selected);
+                    now.setCourseName(enterClass.getText().toString());
+                    now.setCourseTime(enterTime.getText().toString());
+                    now.setCourseTeacher(enterInstructor.getText().toString());
+                    now.setCourseBuilding(enterLocation.getText().toString());
+                    now.setCourseDaysOfWeek(enterDaysOfWeek.getText().toString());
+                    adapter.notifyDataSetChanged();
+
+                    enterClass.setText("");
+                    enterTime.setText("");
+                    enterInstructor.setText("");
+                    enterLocation.setText("");
+                    enterDaysOfWeek.setText("");
+                    selected = -1;
+                }
+            }
+        });
+
+        Button del = view.findViewById(R.id.deleteClass);
+        del.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (selected != -1) {
+                    backingList.remove(selected);
+                    adapter.notifyDataSetChanged();
+                    selected = -1;
+
+                    enterClass.setText("");
+                    enterTime.setText("");
+                    enterInstructor.setText("");
+                    enterLocation.setText("");
+                    enterDaysOfWeek.setText("");
+                }
             }
         });
 
